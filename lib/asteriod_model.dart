08cddb2e7cbd6dabd7ca4f5bbae7ef3b195ddd88
safadtm <safadtm2004@gmail.com>
2022-11-10 10:13:34 +0530

    Asteroids movement created

--- lib/asteriod_model.dart
+++ lib/asteriod_model.dart
@@ -1,13 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/animation.dart';
 import 'package:flutter/cupertino.dart';
 
 class AsteriodData {
   final Size size;
   Alignment alignment;
+  final String path = "assets/asteroid.png";
+
   AsteriodData({
     required this.size,
     required this.alignment,
   });
-  final String path = "assets/";
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:space_attackers/asteriod_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,6 +20,31 @@ class _HomeScreenState extends State<HomeScreen> {
   double gravity = -4.9;
   bool isGameRunning = false;
 
+  List<AsteriodData> asteroidData = [];
+
+  List<AsteriodData> setAsteroidData() {
+    List<AsteriodData> data = [
+      AsteriodData(
+        size: const Size(40, 60),
+        alignment: const Alignment(2, 0.7),
+      ),
+      AsteriodData(
+        size: const Size(80, 100),
+        alignment: const Alignment(1.5, -0.5),
+      ),
+      AsteriodData(
+        size: const Size(40, 50),
+        alignment: const Alignment(3, -0.2),
+      ),
+      AsteriodData(
+        size: const Size(60, 30),
+        alignment: const Alignment(2.2, 0.2),
+      ),
+    ];
+
+    return data;
+  }
+
   void startGame() {
     isGameRunning = true;
 
@@ -31,6 +58,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
           shipY = initialPosition - maxHeight;
         });
+        moveAsteriod();
       },
     );
   }
@@ -40,6 +68,46 @@ class _HomeScreenState extends State<HomeScreen> {
       time = 0;
       initialPosition = shipY;
     });
+  }
+
+  double generateRandomNumber() {
+    Random rand = Random();
+    double randomDouble = rand.nextDouble() * (-1.0 - 1.0) + 1.0;
+    return randomDouble;
+  }
+
+  void moveAsteriod() {
+    Alignment asteriod1 = asteroidData[0].alignment;
+    Alignment asteriod2 = asteroidData[1].alignment;
+    Alignment asteriod3 = asteroidData[2].alignment;
+    Alignment asteriod4 = asteroidData[3].alignment;
+
+    if (asteriod1.x > -1.4) {
+      asteroidData[0].alignment = Alignment(asteriod1.x - 0.02, asteriod1.y);
+    } else {
+      asteroidData[0].alignment = Alignment(2, generateRandomNumber());
+    }
+    if (asteriod2.x > -1.4) {
+      asteroidData[1].alignment = Alignment(asteriod2.x - 0.02, asteriod2.y);
+    } else {
+      asteroidData[1].alignment = Alignment(1.5, generateRandomNumber());
+    }
+    if (asteriod3.x > -1.4) {
+      asteroidData[2].alignment = Alignment(asteriod3.x - 0.02, asteriod3.y);
+    } else {
+      asteroidData[2].alignment = Alignment(3, generateRandomNumber());
+    }
+    if (asteriod4.x > -1.4) {
+      asteroidData[3].alignment = Alignment(asteriod4.x - 0.02, asteriod4.y);
+    } else {
+      asteroidData[3].alignment = Alignment(2.2, generateRandomNumber());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    asteroidData = setAsteroidData();
   }
 
   @override
@@ -66,6 +134,54 @@ class _HomeScreenState extends State<HomeScreen> {
                   decoration: const BoxDecoration(
                     image: DecorationImage(
                       image: AssetImage("assets/ship1.png"),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: asteroidData[0].alignment,
+                child: Container(
+                  height: asteroidData[0].size.height,
+                  width: asteroidData[0].size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(asteroidData[0].path),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: asteroidData[1].alignment,
+                child: Container(
+                  height: asteroidData[1].size.height,
+                  width: asteroidData[1].size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(asteroidData[2].path),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: asteroidData[2].alignment,
+                child: Container(
+                  height: asteroidData[2].size.height,
+                  width: asteroidData[2].size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(asteroidData[2].path),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: asteroidData[3].alignment,
+                child: Container(
+                  height: asteroidData[3].size.height,
+                  width: asteroidData[3].size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(asteroidData[3].path),
                     ),
                   ),
                 ),
